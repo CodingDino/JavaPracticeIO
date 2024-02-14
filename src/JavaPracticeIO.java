56206deacecede9a76e83031a8b6efe3a9fa1617
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2024-02-14 11:59:39 +0000

    Added variable output and input

--- src/JavaPracticeIO.java
+++ src/JavaPracticeIO.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; // Needed for input
+
 
 public class JavaPracticeIO {
 
@@ -5,8 +7,17 @@ public class JavaPracticeIO {
 	
 	public static void main(String[] args) {
 		
+		// Output Example
 		System.out.println("Hello world!");
-	
+		
+		// Variable used for output
+		String toPrint = "What is your favourite colour?";
+		System.out.println(toPrint);
+		
+		// Input example
+		Scanner input = new Scanner(System.in); 
+		String colourChoice = input.nextLine();
+		System.out.println("Ah, I like "+colourChoice+" too!");
 	}
 	
 	
